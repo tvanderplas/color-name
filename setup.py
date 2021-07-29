8bd871f8bd2b348339965a4d59101db952320ee8
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-07-28 20:07:26 -0400

    publish to PYPI and make things work

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="color-name",
-    version="0.1.0",
+    version="0.1.2",
     description="Tells you what name a color has",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,5 +25,5 @@ setup(
     ],
     packages=["colorname"],
     include_package_data=True,
-    install_requires=["PIL"],
+    install_requires=["pillow"],
 )
