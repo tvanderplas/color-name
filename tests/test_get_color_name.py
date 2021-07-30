232f473eb370823243de26b4f334835d36455bb5
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-07-30 18:09:40 -0400

    add tests

--- tests/test_get_color_name.py
+++ tests/test_get_color_name.py
@@ -4,20 +4,56 @@ import context
 from colorname import get_color_name # pylint: disable=import-error
 
 class TestGetColorName(unittest.TestCase):
-    
+
     def test_gets_red(self):
         cases = [
-            (102, 0, 0),
-            (153, 0, 0),
-            (204, 0, 0),
-            (255, 0, 0),
-            (255, 51, 51),
+            (102, 0,   0),
+            (153, 0,   0),
+            (204, 0,   0),
+            (255, 0,   0),
+            (255, 51,  51),
             (255, 102, 102),
             (255, 153, 153),
             (255, 204, 204),
         ]
         for case in cases:
             expected = 'red'
+            actual = get_color_name(*case)
+            msg = f'actual result for {case} was {actual}, expected {expected}'
+            self.assertEqual(actual, expected, msg=msg)
+
+
+    def test_gets_green(self):
+        cases = [
+            (0,   102, 0),
+            (0,   153, 0),
+            (0,   204, 0),
+            (0,   255, 0),
+            (51,  255, 51),
+            (102, 255, 102),
+            (153, 255, 153),
+            (204, 255, 204),
+        ]
+        for case in cases:
+            expected = 'green'
+            actual = get_color_name(*case)
+            msg = f'actual result for {case} was {actual}, expected {expected}'
+            self.assertEqual(actual, expected, msg=msg)
+
+
+    def test_gets_blue(self):
+        cases = [
+            (0,   0,   102),
+            (0,   0,   153),
+            (0,   0,   204),
+            (0,   0,   255),
+            (51,  51,  255),
+            (102, 102, 255),
+            (153, 153, 255),
+            (204, 204, 255),
+        ]
+        for case in cases:
+            expected = 'blue'
             actual = get_color_name(*case)
             msg = f'actual result for {case} was {actual}, expected {expected}'
             self.assertEqual(actual, expected, msg=msg)
