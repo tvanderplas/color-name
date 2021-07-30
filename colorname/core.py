6ea187bf5c369d3414ebe4b0b3256ea081d4a924
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-07-30 16:06:38 -0400

    make test pass

--- colorname/core.py
+++ colorname/core.py
@@ -1,14 +1,15 @@
 
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 
 def get_color_name(r, g, b):
+    h, s, v = Image.new('RGB', (1, 1), (r, g, b)).convert('HSV').getdata()[0]
     if r < 15 and g < 15 and b < 15:
         return "black"
     elif r > 220 and g > 220 and b > 220:
         return "white"
     elif r < g and g > 120 and b < g:
         return "green"
-    elif r > 120 and g < r and b < r:
+    elif h < 30 or h >= 330:
         return "red"
     elif r < b and g < b and b > 120:
         return "blue"
@@ -25,3 +26,6 @@ def get_pixel_color_name(x, y):
     '''Returns the name of a color at the given pixel coordinates'''
     color = get_pixel_color(x, y)
     return get_color_name(*color)
+
+if __name__ == '__main__':
+    get_color_name(255, 0, 0)
