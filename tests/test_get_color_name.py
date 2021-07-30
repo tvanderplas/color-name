e2d34f54b7a9c086432f2e438b137184bd338b46
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-07-30 13:54:36 -0400

    add failing test

--- tests/test_get_color_name.py
+++ tests/test_get_color_name.py
@@ -7,10 +7,20 @@ class TestGetColorName(unittest.TestCase):
     
     def test_gets_red(self):
         cases = [
+            (102, 0, 0),
+            (153, 0, 0),
+            (204, 0, 0),
             (255, 0, 0),
+            (255, 51, 51),
+            (255, 102, 102),
+            (255, 153, 153),
+            (255, 204, 204),
         ]
         for case in cases:
-            self.assertEqual(get_color_name(*case), 'red')
+            expected = 'red'
+            actual = get_color_name(*case)
+            msg = f'actual result for {case} was {actual}, expected {expected}'
+            self.assertEqual(actual, expected, msg=msg)
 
 
 if __name__ == '__main__':
